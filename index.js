54af5c302db790eca1b483079ea68bd9d450662f
Evgen Bielich <belichevgenij@gmail.com>
2021-09-07 22:29:14 +0300

    3-2

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+// !MODULE
+
 // import './js/module2-1-arrays.js';
 // import './js/module2-2-function.js';
 
 
-import './js/module3-1-objects.js';
+// import './js/module3-1-objects.js';
 // import './js/module-3-destructing.js';
+// import './js/module-3-spread-rest.js';
+
+
+// !PRACTIC
+
+import './practic/module-3-practic.js';
